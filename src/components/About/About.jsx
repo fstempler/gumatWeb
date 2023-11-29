c0b44edab181a5cbea55b00452f87d24e8aaa402
fstempler <zackmango@gmail.com>
2023-11-29 10:24:38 -0300

    Fix About Style  / Fix Navbar Toggler Aligment

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,18 +16,16 @@ const About = () => {
             </div>
             <div className='about__Data-Container'>
 
-            <div className='row'>
+                <div className='row'>
                     <div className='col-lg-6 order-lg-1 order-2 about__Text-Container'>
-                        
                         <p className='about__Text'>
-                        {t('aboutText')}
-
+                            {t('aboutText')}
                         </p>
-                        
                     </div>
                     <div className='col-lg-6 order-lg-2 order-1 about__Video-Container'>
                         <video src={video} className='about__Video' autoPlay loop muted alt="Video Led Gumat"/>
-                    </div>
+                    </div>                  
+                    
                 </div>
 
             </div>
