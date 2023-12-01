2171a64e801f1a92afa46fd733b7ceb117ea6412
fstempler <zackmango@gmail.com>
2023-12-01 14:36:46 -0300

    Fix Play Video iOS V

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,30 @@
 import './header.css'
 import bgVideo from '../../assets/header/VID-6.mp4'
 import { useTranslation } from 'react-i18next';
+import { useEffect, useRef } from 'react';
 
 const Header = () => {
 
     const { t } = useTranslation();
 
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        const playVideo = () => {
+            if (videoRef.current) {
+                videoRef.current.play();
+            }
+        };
+
+        // Puedes llamar a playVideo después de algún evento de usuario
+        // o después de un tiempo específico (por ejemplo, después de 1 segundo).
+        setTimeout(playVideo, 1000);
+    }, []);
+
     return (
        <div className='header__MainContainer' id='Header'>
             <div className='header__Overlay'></div>
-                <video src={bgVideo} className='header__Video'autoPlay loop muted playsInline controls="true" alt="Gumat video fábrica"/>
+                <video src={bgVideo} ref={videoRef} className='header__Video'autoPlay loop muted playsInline alt="Gumat video fábrica"/>
                 <div className='header__Text-Container'>
                     <h1 className='header__Title'>{t('headerTitle')}</h1>                    
                 </div>            
