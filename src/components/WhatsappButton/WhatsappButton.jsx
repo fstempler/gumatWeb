3129a757005e52aca8ba05e63a94e570c9aba31f
fstempler <zackmango@gmail.com>
2024-03-06 18:29:30 -0300

    Add Whatsapp Btn

--- src/components/WhatsappButton/WhatsappButton.jsx
+++ src/components/WhatsappButton/WhatsappButton.jsx
@@ -3,8 +3,13 @@ import './whatsappButton.css'
 
 const WhatsappButton = () => {
     return (
-        <div className="whatsappButton__Container">
-            <a href="https://wa.me/5491133412690" target='_blank' rel="noreferrer"><img src={WappButton} className='whatsappButton__Image'/></a>
+        <div>
+            <div className="whatsappButton__Container">
+                <a href="https://wa.me/5491133412690" target='_blank' rel="noreferrer"><span className='whatsappButton__Title'>Ventas 2</span><img src={WappButton} className='whatsappButton__Image'/></a>            
+            </div>
+            <div className="whatsappButton__Container2">
+                <a href="https://wa.me/5491139433311" target='_blank' rel="noreferrer"><span className='whatsappButton__Title'>Ventas 1</span><img src={WappButton} className='whatsappButton__Image'/></a>            
+            </div>
         </div>
     )
 }
